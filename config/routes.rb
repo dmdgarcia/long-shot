d0d92af67bd4a7ddc66585388634a1bbfe259b55
bogdanmatei <matei90bogdan@yahoo.com>
2020-07-13 23:52:56 +0100

    Created teams controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-
-  resources :games
   
-  root "games#index"
+  root to: "games#index"
+  resources :games
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
