af3dfc80e0a2741ac78a893972eb910d06aa01dc
dmdgarcia <davidmd.garcia@gmail.com>
2020-07-14 14:26:16 +0900

    update model game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,9 @@
 class Game < ApplicationRecord
-  has_one :home_team, class_name: 'Team', foreign_key: 'home_id'
-  has_one :away_team, class_name: 'Team', foreign_key: 'away_id'
+  has_one :home_team, class_name: 'Team', foreign_key: 'home_team_id'
+  has_one :away_team, class_name: 'Team', foreign_key: 'away_team_id'
+
+  def teams
+    [away_team, home_team]
+  end
 
 end
