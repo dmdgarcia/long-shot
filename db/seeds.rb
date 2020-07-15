a6c52694129c830347459974ee08c07dce1203d6
bogdanmatei <matei90bogdan@yahoo.com>
2020-07-15 20:05:36 +0100

    Parsed file with seeds, added main div in games index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+
 
 puts "Clearing database..."
 Team.destroy_all
 Game.destroy_all
 
+# league_url = "https://raw.githubusercontent.com/openfootball/football.json/master/2019-20/en.1.clubs.json"
+# get_league = open(league_url).read
+
+# JSON.parse(get_league)['clubs'].each do |team|
+#     Team.create(name: team["name"])
+#     Game.create(home_id: team["name"], away_id: team["name"])
+# end
+
 t1 = Team.create(name: 'Liverpool')
 t2 = Team.create(name: 'Manchester City')
-
 game = Game.create(home_id: t1.id, away_id: t2.id)
 
-puts "Created #{Game.count}"
+puts "Created #{Game.count} games"
+puts "Created #{Team.count} teams"
