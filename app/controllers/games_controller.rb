90469d536a44bdc64e9267e986abde6ccbcfeb07
dmdgarcia <davidmd.garcia@gmail.com>
2020-07-15 13:13:33 +0900

    update model and controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,13 @@ class GamesController < ApplicationController
 
 def index
     @games = Game.all
+    
+end
+
+def game
+    @games.each do |game|
+        game.home_id - game.away_id
+    end
 end
 
 
