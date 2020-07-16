800152dcffbbb9bdc14d1b027d0b4b1789e8bcc4
bogdanmatei <matei90bogdan@yahoo.com>
2020-07-16 12:37:41 +0100

    added bets to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_045852) do
+ActiveRecord::Schema.define(version: 2020_07_16_042811) do
+
+  create_table "bets", force: :cascade do |t|
+    t.integer "goals"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.integer "home_id"
