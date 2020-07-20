54c1a3dca4222abb4718d23e5354dad9af830361
bogdanmatei <matei90bogdan@yahoo.com>
2020-07-20 23:58:24 +0100

    Added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
-  has_many :games
+  has_many :bets
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :first_name, :last_name, :username, :email, presence: true
+  validates :username, :email, uniqueness: true
+  validates :email, format: { with: /\A[a-z0-9.]+@[a-z]+\.[a-z]{2,3}\z/i }
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
